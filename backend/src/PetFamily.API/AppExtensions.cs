f609876b07bce428c3a331c1b766a39db9071ea8
ilnur767 <email@email.com>
2025-02-06 11:33:24 +0500

    add serilog, exception middleware

---
+++ backend/src/PetFamily.API/AppExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetFamily.Infrastructure;
+
+namespace PetFamily.API;
+
+public static class AppExtensions
+{
+    public static async void ApplyMigration(this IApplicationBuilder app)
+    {
+        await using var scope = app.ApplicationServices.CreateAsyncScope();
+
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        await dbContext.Database.MigrateAsync();
+    }
+}
--- backend/src/PetFamily.API/Program.cs
+++ backend/src/PetFamily.API/Program.cs
@@ -1,23 +1,39 @@
+using PetFamily.API;
+using PetFamily.API.Middlewares;
 using PetFamily.Application.Inject;
 using PetFamily.Infrastructure.Inject;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") ?? throw new ArgumentNullException("Seq"))
+    .CreateLogger();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+builder.Services.AddHttpLogging(o => { });
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
 
+builder.Services.AddSerilog();
+
 var app = builder.Build();
+app.UseExceptionMiddleware();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ApplyMigration();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 app.MapControllers();
