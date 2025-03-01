17acaafdb7a554d59642fe122980037dc61ee29b
ilnur767 <email@email.com>
2025-03-01 17:28:38 +0500

    Add MinioProvider, add test file controller

---
+++ backend/src/PetFamily.Application/FileProvider/IFileProvider.cs
@@ -0,0 +1,19 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Domain.Common;
+
+namespace PetFamily.Application.FileProvider;
+
+public interface IFileProvider
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="fileData"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task<Result<string, Error>> UploadFile(FileData fileData, CancellationToken cancellationToken);
+
+    public Task<Result<string, Error>> GetPresignedUrl(string fileName, string bucketName,
+        CancellationToken cancellationToken);
+
+    public Task<UnitResult<Error>> DeleteFile(string fileName, string bucketName, CancellationToken cancellationToken);
+}
---
+++ backend/src/PetFamily.Application/Files/AddFile/AddFileHandler.cs
@@ -0,0 +1,30 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Application.FileProvider;
+using PetFamily.Domain.Common;
+
+namespace PetFamily.Application.Files.AddFile;
+
+public class AddFileHandler
+{
+    private readonly IFileProvider _fileProvider;
+
+    public AddFileHandler(IFileProvider fileProvider)
+    {
+        _fileProvider = fileProvider;
+    }
+
+    public async Task<Result<string, Error>> Handle(AddFileCommand fileCommand, CancellationToken cancellationToken)
+    {
+        var result = await _fileProvider.UploadFile(
+            new FileData(fileCommand.Stream, fileCommand.BucketName, fileCommand.FileName), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return result.Error;
+        }
+
+        return result.Value;
+    }
+}
+
+public record AddFileCommand(Stream Stream, string FileName, string BucketName);
