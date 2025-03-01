bfd652d33eaa749cc9b80b82c669b581cf373c99
ilnur767 <email@email.com>
2025-03-01 17:34:54 +0500

    fix

--- backend/src/PetFamily.Application/FileProvider/IFileProvider.cs
+++ backend/src/PetFamily.Application/FileProvider/IFileProvider.cs
@@ -6,14 +6,28 @@ namespace PetFamily.Application.FileProvider;
 public interface IFileProvider
 {
     /// <summary>
+    ///     Загрузить файл в хранилище.
     /// </summary>
-    /// <param name="fileData"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
+    /// <param name="fileData">Информация о файле.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns>Наименование файла в хранилище.</returns>
     public Task<Result<string, Error>> UploadFile(FileData fileData, CancellationToken cancellationToken);
 
+    /// <summary>
+    ///     Получить ссылку на скачивание файла из хранилища.
+    /// </summary>
+    /// <param name="fileName">Наименование файла.</param>
+    /// <param name="bucketName">Наименование бакета.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns>Ссылка на скачивание файла.</returns>
     public Task<Result<string, Error>> GetPresignedUrl(string fileName, string bucketName,
         CancellationToken cancellationToken);
 
+    /// <summary>
+    ///     Удалить файл из хранилища.
+    /// </summary>
+    /// <param name="fileName">Наименование файла.</param>
+    /// <param name="bucketName">Наименование бакета.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
     public Task<UnitResult<Error>> DeleteFile(string fileName, string bucketName, CancellationToken cancellationToken);
 }
